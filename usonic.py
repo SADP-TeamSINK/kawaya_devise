d51184aa34999075320b6777e664fe7698c32f40
kirikei <kiritoshi@db.soc.i.kyoto-u.ac.jp>
2014-09-09 14:26:56 -0900

    fundamental functions are completed?

--- usonic.py
+++ usonic.py
@@ -2,6 +2,7 @@
     # coding: UTF-8
 import numpy as np
 import cv2
+import time
 # remember to change the GPIO values below to match your sensors
 # GPIO output = the pin that's connected to "Trig" on the sensor
 # GPIO input = the pin that's connected to "Echo" on the sensor
@@ -22,10 +23,14 @@ def onTrackbarSlide(pos):
     cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos)
     updatelock = False
 
+#センサーで距離を読み取る関数
 def reading(sensor):
-    import time
     import RPi.GPIO as GPIO
     
+    signalon = 0
+#signaloffがechoの一番目のwhileで上手く代入されないことがあるので
+#初期化しておく
+    signaloff = 0
     # Disable any warning message such as GPIO pins in use
     GPIO.setwarnings(False)
     
@@ -34,7 +39,8 @@ def reading(sensor):
     # reference in this code is 25, which is the number of the GPIO 
     # port and not the number of the physical pin
     GPIO.setmode(GPIO.BCM)
-    
+
+    #ここから内部
     if sensor == 0:
         
         # point the software to the GPIO pins the sensor is using
@@ -47,7 +53,7 @@ def reading(sensor):
         
         # found that the sensor can crash if there isn't a delay here
         # no idea why. If you have odd crashing issues, increase delay
-        time.sleep(0.3)
+        time.sleep(0.3)#センサーとの通信のためのdelay
         
         # sensor manual says a pulse ength of 10Us will trigger the 
         # sensor to transmit 8 cycles of ultrasonic burst at 40kHz and 
@@ -57,44 +63,56 @@ def reading(sensor):
         # wait for 10 microseconds, then stop the pulse. This will 
         # result in the pulse length being 10Us.
         
+        #前提としてtrigを10UsだけHighにすると距離の測定開始
+        #対象の距離が長いほど返ってくる音波も長くなる
+        #音波を受け取るechoがHighになった時間で距離が計測できる
+
         # start the pulse on the GPIO pin 
         # change this value to the pin you are using
         # GPIO output = the pin that's connected to "Trig" on the sensor
-        GPIO.output(17, True)
+        GPIO.output(17, True)#trigをHighにする
         
         # wait 10 micro seconds (this is 0.00001 seconds) so the pulse
         # length is 10Us as the sensor expects
-        time.sleep(0.00001)
+        time.sleep(0.00001)#この秒数だけHighに
         
         # stop the pulse after the time above has passed
         # change this value to the pin you are using
         # GPIO output = the pin that's connected to "Trig" on the sensor
-        GPIO.output(17, False)
+        GPIO.output(17, False)#trigをLowに
+
+        #計測開始！
 
         # listen to the input pin. 0 means nothing is happening. Once a
         # signal is received the value will be 1 so the while loop
         # stops and has the last recorded time the signal was 0
         # change this value to the pin you are using
         # GPIO input = the pin that's connected to "Echo" on the sensor
-        while GPIO.input(27) == 0:
-          signaloff = time.time()
+        signaloff = time.time()
+        while GPIO.input(27) == 0:#パルスを受け取らない限り(待ち状態)
+          signaloff = time.time()#signaloffに時間を代入
         
         # listen to the input pin. Once a signal is received, record the
         # time the signal came through
         # change this value to the pin you are using
         # GPIO input = the pin that's connected to "Echo" on the sensor
-        while GPIO.input(27) == 1:
-          signalon = time.time()
+        while GPIO.input(27) == 1:#パルスを受け取っている限り
+          signalon = time.time()#signalonに時間を代入
         
         # work out the difference in the two recorded times above to 
         # calculate the distance of an object in front of the sensor
-        timepassed = signalon - signaloff
+        #time.sleep(0.001)
+
+        #print "signaloff:",signaloff
+
+        timepassed = signalon - signaloff#二つの時間差から距離を求める
         
         # we now have our distance but it's not in a useful unit of
         # measurement. So now we convert this distance into centimetres
         distance = timepassed * 17000
         
         # return the distance of an object in front of the sensor in cm
+        print distance
         return distance
         
         # we're no longer using the GPIO, so tell software we're done
@@ -103,52 +121,62 @@ def reading(sensor):
     else:
         print "Incorrect usonic() function varible."
 
-        
-if(reading(0)<100):
-    print "true"
+
+while(True):
+    if(reading(0)<100):#距離が100cm以内であれば
+        print "true"
 
 # 名前付きWindowを定義する
-    cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
+        cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
 
 # MP4ファイルのフレーム数を取得する
-    frames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-
+        frames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        print "frame:",frames
 # フレーム数が1以上ならトラックバーにセットする
-    if (frames > 0):
-        cv2.createTrackbar(trackbarname, windowname, 0, frames, onTrackbarSlide)
-
+        if (frames > 0):
+            cv2.createTrackbar(trackbarname, windowname, 0, frames, onTrackbarSlide)
+            print "1"
 # MP4ファイルを開いている間は繰り返し（最後のフレームまで読んだら終わる）
-    while(cap.isOpened()):
-
+            while(cap.isOpened()):
+                print "2"
     # トラックバー更新中は描画しない
-        if (updatelock):
-            continue
+                if (updatelock):
+                    continue
 
-    # １フレーム読む
-        ret, frame = cap.read()
+        # １フレーム読む
+                ret, frame = cap.read()
 
-        # 読めなかったら抜ける
-        if ret == False:
-            break
+                # 読めなかったら抜ける
+                if ret == False:
+                    print "3"
+                    break
 
-        # 画面に表示
-        cv2.imshow(windowname,frame)
+                # 画面に表示
+                cv2.imshow(windowname,frame)
 
-        # 現在のフレーム番号を取得
-        curpos = int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
+                # 現在のフレーム番号を取得
+                curpos = int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
+                
+                # トラックバーにセットする（コールバック関数が呼ばれる）
+                cv2.setTrackbarPos(trackbarname, windowname, curpos)
 
-    # トラックバーにセットする（コールバック関数が呼ばれる）
-        cv2.setTrackbarPos(trackbarname, windowname, curpos)
+                # qを押したら抜ける
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                # 人が100cmより離れたら抜ける
+                if (reading(0)>100):
+                    print "testes"
+                    break
 
-    # qを押したら抜ける
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
-    # AVIファイルを解放
-    cap.release()
+                # mp4ファイルを解放
+           # cap.release()
 
-    # Windowを閉じる
-    cv2.destroyAllWindows()
+                # Windowを閉じる
+            print "4"
+            cv2.destroyAllWindows()
 
-else:
-    print "false"
+    else:
+        print "false"
+
+    time.sleep(3)#３秒ごとに距離を測る
