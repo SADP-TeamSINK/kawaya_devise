cc2e1a420b058fe2a5b22afa870ccdffb978bcbb
kirikei <kirikei@users.noreply.github.com>
2014-10-31 20:41:31 +0900

    Update usonic.py

--- usonic.py
+++ usonic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-
+    # coding: UTF-8
+import numpy as np
+import cv2
 # remember to change the GPIO values below to match your sensors
 # GPIO output = the pin that's connected to "Trig" on the sensor
 # GPIO input = the pin that's connected to "Echo" on the sensor
@@ -83,10 +85,69 @@ def reading(sensor):
         GPIO.cleanup()
 
     else:
-        print("Incorrect usonic() function varible.")
+        print "Incorrect usonic() function varible."
 
         
 if(reading(0)<100):
-    print("true")
+    print "true"
+    updatelock = False # トラックバー処理中のロックフラグ
+    windowname = 'frame' # Windowの名前
+    trackbarname = 'Position' # トラックバーの名前
+
+    # MP4ファイルを読む
+    # MP4は適当な長さのサンプルをインターネットから拾ってくる
+    # 参考:http://www.engr.colostate.edu/me/facil/dynamics/avis.htm
+    # http://www.gomplayer.jp/player/support/sample.htmlから取ってきました
+    cap = cv2.VideoCapture('mp4_h264_aac.mp4')
+
+# トラックバーを動かしたときに呼び出されるコールバック関数の定義
+    def onTrackbarSlide(pos):
+        updatelock = True
+        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos)
+        updatelock = False
+
+# 名前付きWindowを定義する
+    cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
+
+# MP4ファイルのフレーム数を取得する
+    frames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+
+# フレーム数が1以上ならトラックバーにセットする
+    if (frames > 0):
+        cv2.createTrackbar(trackbarname, windowname, 0, frames, onTrackbarSlide)
+
+# MP4ファイルを開いている間は繰り返し（最後のフレームまで読んだら終わる）
+    while(cap.isOpened()):
+
+    # トラックバー更新中は描画しない
+        if (updatelock):
+            continue
+
+    # １フレーム読む
+        ret, frame = cap.read()
+
+        # 読めなかったら抜ける
+        if ret == False:
+            break
+
+        # 画面に表示
+        cv2.imshow(windowname,frame)
+
+        # 現在のフレーム番号を取得
+        curpos = int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
+
+    # トラックバーにセットする（コールバック関数が呼ばれる）
+        cv2.setTrackbarPos(trackbarname, windowname, curpos)
+
+    # qを押したら抜ける
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # AVIファイルを解放
+    cap.release()
+
+    # Windowを閉じる
+    cv2.destroyAllWindows()
+
 else:
-    print("false")
+    print "false"
